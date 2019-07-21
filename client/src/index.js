6d7a7649f2765465934418b5b736d6925f2419c0
jussaurabh <pawar.saurabh95@gmail.com>
2019-07-21 20:38:11 +0530

    updated server.js and package.json

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './store/Reducers/rootReducer';
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+const store = createStore(
+	rootReducer,
+	compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
 
 ReactDOM.render(
 	<BrowserRouter>
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // Import Routes
 const item = require('./server/routes/api/item');
@@ -20,6 +21,16 @@ mongoose
 
 // Routes
 app.use('/api/items', item);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
